f46e59d6b79aa826b386d1a73a67b85d58e35a14
Pratap <suryapratapsing8@gmail.com>
2022-06-19 09:15:42 +0530

    "improve the game"

---
+++ Rock_Paper_Scissor.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public Rock_Paper_Scissor.java{
+    public void main(Str[],args){
+        Scanner sc = sc.
+        System.out.println("Please choose one condition:");
+        System.out.println("Press 0 for Rock\nPress 1 for Paper\nPress 2 for Scissor");
+        System.out.print("Enter your choice");
+        int user = 
+    }
+}
